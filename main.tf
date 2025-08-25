fbefd3ab2ecf567133ecd3c7218327642803d784
Chad Cottle <cottle.chad@yahoo.com>
2025-08-25 15:40:27 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-05d9c3ee13ed819d4"
+
+  tags = {
+    Name = "learn-terraform"
+  }
+}
+
